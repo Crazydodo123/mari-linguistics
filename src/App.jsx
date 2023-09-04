91ad583db3ba7fc7d37f6c15ce02c483aec9ec77
Crazydodo123 <peizheg@gmail.com>
2023-09-04 17:03:05 -0400

    add start section

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <nav id="navbar">
+          <img src={logo} id="logo" />
+          <ul>
+            <li><a class="navlink" href="#welcome-section">Welcome</a></li>
+            <li><a class="navlink" href="#projects">About Me</a></li>
+            <li><a class="navlink" href="#about-me">More</a></li>
+          </ul>
+          <button id="sign-up">Sign Up!</button>
+        </nav>
+      </header>
+      <main>
+        <div id="official">
+          The Official Linguistics Club at Marianopolis 
+        </div>
+      </main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import firebase from 'firebase/app'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
